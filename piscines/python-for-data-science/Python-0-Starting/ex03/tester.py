c49432a8b331d1c5542474ab4b9f04ccfac5aea1
Basil ALBADER <baalbade@student.42.fr>
2024-09-02 11:34:23 +0200

    ex03

---
+++ piscines/python-for-data-science/Python-0-Starting/ex03/tester.py
@@ -0,0 +1,16 @@
+from NULL_not_found import NULL_not_found
+
+Nothing = None
+Garlic = float("NaN")
+Zero = 0
+Empty = ''
+Fake = False
+
+NULL_not_found(Nothing)
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+# ! Can't pass a string to NULL_not_found and get type not found
+# print(NULL_not_found("Brian"))
+print(NULL_not_found({}))
