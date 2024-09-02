74b74555c64d5b3ac11c29131c3970b71ecb1bef
Basil ALBADER <baalbade@student.42.fr>
2024-09-02 11:54:40 +0200

    WIP

--- piscines/python-for-data-science/Python-0-Starting/ex04/whatis.py
+++ piscines/python-for-data-science/Python-0-Starting/ex04/whatis.py
@@ -7,7 +7,8 @@ def main():
     Try to convert the argument to an integer
     Check if the number is even or odd and print the result
     '''
-    assert len(sys.argv) == 2, "AssertionError: Please provide exactly one argument."
+    assert len(sys.argv) == 2
+    "AssertionError: Please provide exactly one argument."
 
     try:
         number = int(sys.argv[1])
@@ -18,6 +19,7 @@ def main():
         print("Even")
     else:
         print("Odd")
+
 
 if __name__ == "__main__":
     main()
