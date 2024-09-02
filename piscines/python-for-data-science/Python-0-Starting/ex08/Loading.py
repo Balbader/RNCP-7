de9a33b1c39a1ea34864e4fcadf68f78038c27c5
balbader <basilalbapro@gmail.com>
2024-09-02 19:41:51 +0200

    WIP

--- piscines/python-for-data-science/Python-0-Starting/ex08/Loading.py
+++ piscines/python-for-data-science/Python-0-Starting/ex08/Loading.py
@@ -1,21 +1,28 @@
 import time
 import sys
 
+'''
+    the following code is a simple implementation of the tqdm library
+    the tqdm library is used to show a progress bar while condition:
+    passiterating over a list or a range
+    the code is a generator function that takes a list
+    or a range as an argument
+    it calculates the progress and displays a progress bar in the console
+'''
+
 
 def ft_tqdm(lst: range) -> None:
     total = len(lst)  # Total number of elements in the list/range
-    start_time = time.time()  # Record the start time
 
     for i, item in enumerate(lst):
         # Calculate the progress
-        elapsed_time = time.time() - start_time
         progress = (i + 1) / total
         percent = progress * 100
         bar_length = 50
         bar = '=' * int(bar_length * progress) + '-' * (bar_length - int(bar_length * progress))
 
         # Print the progress bar
-        sys.stdout.write(f'\r{percent:.2f}%|{bar}|Elapsed Time: {elapsed_time:.2f}s')
+        sys.stdout.write(f'\r{percent:.0f}%|{bar}| {total:.0f}/{total:.0f}')
         sys.stdout.flush()
 
         yield item  # Yield the current item
