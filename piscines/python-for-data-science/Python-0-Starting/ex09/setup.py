bac30a570c3f299908013fd3db160c08739a2646
Basil ALBADER <baalbade@student.42.fr>
2024-09-04 10:34:37 +0200

    ex09

---
+++ piscines/python-for-data-science/Python-0-Starting/ex09/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A simple test package',
+    author='Your Name',
+    author_email='baalbade@strudent.42.fr',
+    url='https://github.com/balbader/mypackage',  # URL to the repository
+    License='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
